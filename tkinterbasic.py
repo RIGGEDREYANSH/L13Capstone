b9acb8a2960af9b866ca61fcbac05f53adb08def
RIGGEDREYANSH <ReyanshGour16@gmail.com>
2026-06-18 17:55:32 +0000

    6/18/26

--- tkinterbasic.py
+++ tkinterbasic.py
@@ -9,12 +9,19 @@ class Application(tk.Tk):
         self.label = tk.Label(self, text="Hello, World!")
         self.label.pack(pady=20)
 
+        self.entry = tk.Entry(self, width=30)
+        self.entry.pack(pady=10)
+        self.entry.insert(0, "Type here...")
+
         self.button = tk.Button(self, text="Click Me", command=self.on_button_click)
         self.button.pack(pady=10)
 
+        self.output_label = tk.Label(self, text="Output will appear here.")
+        self.output_label.pack(pady=10)
+
     def on_button_click(self):
-        self.label.config(text="Button clicked!")
         self.button.config(text="Clicked!")
+        self.output_label.config(text=self.entry.get())
 
 if __name__ == "__main__":
     app = Application()
